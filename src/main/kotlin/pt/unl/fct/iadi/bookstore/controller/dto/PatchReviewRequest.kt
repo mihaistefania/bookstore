2ece25d58af9804d8a76d920e706ed6a48919d4b
Mihai Stefania <mihaistefania2002@yahoo.com>
2026-03-16 01:49:16 +0200

    fix

--- src/main/kotlin/pt/unl/fct/iadi/bookstore/controller/dto/PatchReviewRequest.kt
+++ src/main/kotlin/pt/unl/fct/iadi/bookstore/controller/dto/PatchReviewRequest.kt
@@ -1,6 +1,19 @@
-package pt.unl.fct.iadi.bookstore.controller.dto
+import io.swagger.v3.oas.annotations.media.Schema
 
 data class PatchReviewRequest(
+
+    @field:Schema(
+        description = "Rating between 1 and 5",
+        minimum = "1",
+        maximum = "5",
+        example = "4"
+    )
     val rating: Int?,
+
+    @field:Schema(
+        description = "Optional comment",
+        maxLength = 500,
+        example = "Updated comment"
+    )
     val comment: String?
 )
